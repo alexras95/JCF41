bac0bb11ca8c077b4bd7f16d1872de99a1603a8b
mrhaver <f.haver@student.fontys.nl>
2016-03-08 19:38:22 +0100

    Huffman Stap 1+2 verbeterd en gedocumenteerd

--- opdrachten/HuffmanCodering/src/huffmancodering/HuffmanCodering.java
+++ opdrachten/HuffmanCodering/src/huffmancodering/HuffmanCodering.java
@@ -6,9 +6,11 @@
 package huffmancodering;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -20,48 +22,74 @@ public class HuffmanCodering {
             + "Hoedje van, hoedje van\n"
             + "Een, twee, drie, vier\n"
             + "Hoedje van papier\n";
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        HashMap charFrequency = getCharFrequency();
-        String output = "";
-        for(Object o : charFrequency.keySet()){
-            output += String.valueOf(o) + ": " + String.valueOf(charFrequency.get(String.valueOf(o))) + "\n";
+        TreeMap charFrequency = sortMapByValue(getCharFrequency());
+        for (Object o : charFrequency.entrySet()) {
+            System.out.println(o.toString());
         }
-        System.out.println(output);
     }
-    
-    public static HashMap<String, Integer> getCharFrequency(){
+
+    /**
+     * @Author Frank Haver
+     * @Description Het hoedje van papier liedje wordt als char array
+     * doorgelopen en de characters met 0 lengte en enters worden er uit
+     * gehaald. In een HashMap<String, Integer> wordt het character met de
+     * frequentie opgeslagen.
+     * @return
+     */
+    public static HashMap<String, Integer> getCharFrequency() {
         HashMap<String, Integer> charFrequency = new HashMap<>();
-        for(char ch : DEFAULT_TEXT.toCharArray()){
-            if(String.valueOf(ch).length() != 0 && !String.valueOf(ch).equals("\n")){
-                if(charFrequency.containsKey(String.valueOf(ch))){
-                charFrequency.put(String.valueOf(ch), charFrequency.get(String.valueOf(ch)) + 1);
-                }
-                else{
+        for (char ch : DEFAULT_TEXT.toCharArray()) {
+            if (String.valueOf(ch).length() != 0 && !String.valueOf(ch).equals("\n")) {
+                if (charFrequency.containsKey(String.valueOf(ch))) {
+                    charFrequency.put(String.valueOf(ch), charFrequency.get(String.valueOf(ch)) + 1);
+                } else {
                     charFrequency.put(String.valueOf(ch), 1);
                 }
-            }      
-        }
+            }
+        }        
         return charFrequency;
     }
-    
-    public class IntegerComparator implements Comparator {
-        
-        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2 )
-        {
-                return (o1.getValue()).compareTo( o2.getValue() );
+
+    /**
+     * @Author Frank Haver
+     * @Description Er wordt een nieuwe TreeMap<String, Integer> aangemaakt uit
+     * een binnenkomende HashMap<String, Integer>. Deze wordt gesorteerd door
+     * een custom gemaakte comparator met de klasse genaamd ValueComparator.
+     * @param map
+     * @return
+     */
+    public static TreeMap<String, Integer> sortMapByValue(HashMap<String, Integer> map) {
+        TreeMap<String, Integer> result = new TreeMap<>(new ValueComparator(map));
+        result.putAll(map);
+        return result;
+    }
+
+    /**
+     * @Author Frank Haver
+     * @Description Klasse voor het sorteren van een HashMap
+     */
+    public static class ValueComparator implements Comparator<String> {
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        public ValueComparator(HashMap<String, Integer> map) {
+            this.map.putAll(map);
         }
 
         @Override
-        public int compare(Object t, Object t1) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        public int compare(String s1, String s2) {
+            if (map.get(s1) >= map.get(s2)) {
+                return -1;
+            } else {
+                return 1;
+            }
         }
     }
-    
-    
-    
+
 }
